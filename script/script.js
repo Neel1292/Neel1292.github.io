4a17c1c10edb7850d689861fe9610b2b9fb2df98
Neel Prajapti <prajapatineel122002@gmail.com>
2025-04-09 00:09:13 +0530

    Add popup message functionality and enhance contact form validation and email setup

--- script/script.js
+++ script/script.js
@@ -31,7 +31,67 @@ tl.to("nav", {
 
 tl.from(".nav-list h4", {
     top: -100,
-    duration: 0.8,
-    stagger: 0.5,
+    duration: 0.3,
+    stagger: 0.3,
 })
 
+// =========== contact form mail sent ===========  
+
+document.getElementById("contact-form").addEventListener("submit", async function (event) {
+  event.preventDefault(); // Prevent page reload
+
+  let name = document.getElementById("name").value;
+  let email = document.getElementById("email").value;
+  let phone = document.getElementById("number").value;
+  let subject = document.getElementById("subject").value;
+  let message = document.getElementById("message").value;
+
+  // ✅ Basic Validation
+  if (!name || !email || (!phone && phone.length !== 10) || !subject || !message) {
+    showPopup("Please fill out all fields.");
+    return;
+  }
+
+  document.getElementById("name").value = "";
+  document.getElementById("email").value = "";
+  document.getElementById("number").value = "";
+  document.getElementById("subject").value = "";
+  document.getElementById("message").value = ""; 
+
+  let result = await fetch("http://localhost:5787/send-email",{
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      name,
+      email,
+      phone,
+      subject,
+      message,
+    })
+  })
+
+  console.log(result);
+  
+  if(!result.ok) {
+    showPopup("Please try again later", "danger");
+    return
+  }
+  
+  showPopup("Your Response has been recorded", "success")
+
+});
+
+// Function to show the popup
+function showPopup(message, type) {
+  if(type === "danger") {
+    document.getElementById("popup").style.backgroundColor = "#ff3333";
+  }
+  document.getElementById("popup-message").innerText = message;
+  document.getElementById("popup").style.display = "block";
+  setTimeout(closePopup, 5000);
+}
+
+// Function to close the popup
+function closePopup() {
+  document.getElementById("popup").style.display = "none";
+}
